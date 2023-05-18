72ab5ee01e63dc899088629db07e7b8d93a069bc
inakiGPT <ignaciofernandez@atbim.es>
2023-05-18 19:56:57 +0200

    peding add

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,70 @@
-const express =require('express');
-const router =express.Router();
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const User = require('../models/user');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 // ROUTE GET api/users
 // desc test route
 //acces public 
-router.get('/register',(req,res) => {res.send('USE ROUTES')})
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 
-module.exports=router;
+
+})
+
+router.post('/', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ errors: [{ msg: 'invalid credential' }] });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ errors: [{ msg: 'invalid credential' }] });
+        }
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({
+                    message: 'User login',
+                    token: token
+                });
+            }
+        );
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express =require('express');
 const connectDB=require('./config/db');
 const app=express();
 connectDB();
-app.use('api/users',require('./routes/users'));
-app.use('api/auth',require('./routes/auth'));
-app.use('api/profile',require('./routes/profile'));
-app.use('api/users',require('./routes/users'));
-app.use('api/auth',require('./routes/auth'));
+//init middlewere
+
+app.use(express.json({extended:false}));
+app.use('/api/users',require('./routes/users'));
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/profile',require('./routes/profile'));
+app.use('/api/auth',require('./routes/auth'));
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{console.log(`server started on port ${PORT}`);});
 
