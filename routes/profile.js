db7630ff6e5555b8bc7a4968a92a870ef11fd432
inakiGPT <ignaciofernandez@atbim.es>
2023-05-23 18:45:45 +0200

    atrancado

--- routes/profile.js
+++ routes/profile.js
@@ -3,9 +3,10 @@ const router =express.Router();
 const auth=require('../middleware/auth');
 const Profile = require('../models/Profile')
 const User= require('../models/user')
-// ROUTE GET api/profile
-// desc test route
-//acces Private 
+const { check, validationResult } = require('express-validator');
+// bring in normalize to give us a proper url, regardless of what user entered
+
+router.get('/prueba',(req,res) => {res.send('jose mourinho')})
 router.get('/',auth, async(req,res) => {
  try{
     const profile = await Profile.findOne({user:req.user.id}).populate('user',['name','avatar'])
@@ -18,5 +19,46 @@ router.get('/',auth, async(req,res) => {
     res.status(500).send('server Error')
 }
 });
+router.post(
+  '/',
+ [ auth,
+ [ check('status', 'Status is required').notEmpty(),
+  check('skills', 'Skills is required').notEmpty()]],
+  async (req, res) => {
+   
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+   
+    }
+    console.log("hoalsdffd")
+    const {
+      website,
+      skills,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook,
+      // spread the rest of the fields we don't need to check
+      ...rest
+    } = req.body;
+    const profileFields={};
+    profileFields.user=req.user.id;
+    if(company) profileFields.company=company;
+    if(website) profileFields.website=website;
+    if(location) profileFields.location=location;
+    if(bio) profileFields.bio=bio;
+    if(status)profileFields.status=status;
+    if(githubusername)profileFields.githubusername=githubusername;
+    if(skills){
+      console.log(123)
+      profileFields.skills=skills.split(',').map(skill=>skill.trim())
+    }
+    console.log(req.body.status)
+     res.send('hello');
+  }
+);
+
 
 module.exports=router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express =require('express');
 const connectDB=require('./config/db');
-
+const listEndpoints = require('express-list-endpoints');
 const app=express();
 connectDB();
 
@@ -11,6 +11,9 @@ app.use('/api/users',require('./routes/users'));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/profile',require('./routes/profile'));
 app.use('/api/auth',require('./routes/auth'));
+app.use('/api/post',require('./routes/post'))
+const routes = listEndpoints(app);
+console.log(routes);
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{console.log(`server started on port ${PORT}`);});
 
